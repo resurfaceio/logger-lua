fa11f3765e7b7ef341a79c01a36c55a050d42d4a
Anyesh <sthacruz@gmail.com>
2021-10-04 15:14:08 +0545

    feat: Add request and response imple structure

---
+++ usagelogger/http_response_impl.lua
@@ -0,0 +1,15 @@
+
+local HttpResponseImpl = {status=nil, headers=nil, body=nil}
+
+
+function HttpResponseImpl:new(o, status, headers, body)
+    o = o or {}
+    setmetatable(o, self)
+    self.__index = self
+    self.status = status or nil
+    self.headers = headers or {}
+    self.body = body or nil
+    return o
+end
+
+return HttpResponseImpl

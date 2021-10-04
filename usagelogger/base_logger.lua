78c30addff5791e76d453ecee05361519b70a705
Anyesh <sthacruz@gmail.com>
2021-10-04 21:36:42 +0545

    feat: Add env and skip logic

--- usagelogger/base_logger.lua
+++ usagelogger/base_logger.lua
@@ -2,7 +2,7 @@
 http = require("socket.http")
 ltn12 = require("ltn12")
 -- Meta Class
-local BaseLogger = {agent="", enabled=true, queue= nil, url=nil, skip_compression=false, skip_submission=nil,version=nil}
+local BaseLogger = {agent, enabled, queue, url, skip_compression, skip_submission,version}
 local VERSION = "0.1.0"
 
 function BaseLogger:new(o, agent, enabled, queue, url, skip_compression, skip_submission, version)
@@ -10,37 +10,48 @@ function BaseLogger:new(o, agent, enabled, queue, url, skip_compression, skip_su
   setmetatable(o, self)
   self.__index = self
   self.agent = agent
-  self.enabled = enabled
+  self.enabled = enabled or os.getenv("USAGE_LOGGERS_DISABLE") ~= true
   self.queue = queue
-  self.url = url
-  self.skip_compression = skip_compression
-  self.skip_submission = skip_submission
-  self.version = version
+  self.url = url or os.getenv("USAGELOGGER_URL")
+  self.skip_compression = skip_compression or false
+  self.skip_submission = skip_submission or false
+  self.version = version or os.getenv("VERSION") or VERSION
   return o
 end
 
 function BaseLogger:submit(msg)
-    print("called submit with msg: ", msg)
-    local response_body = { }
-    local res, code, response_headers, status = http.request
-    {
-        url = self.url,
-        method = "POST",
-        headers =
-        {
-            ["Connection"] =  "keep-alive",
-            ["User-Agent"]= "Resurface/ " .. VERSION .. " " .. "(" .. self.agent .. ")",
-            ["Content-Type"] = "application/json; charset=UTF-8",
-        },
-        source = ltn12.source.string(msg),
-        sink = ltn12.sink.table(response_body)
-    }
-    return code, response_body
+    if msg == nil or self.skip_submission == true or self.enabled == false then
+        
+    elseif self.queue ~= nil then
+        self.queue:push(msg)
+    else
+        local response_body = { }
+        local res, code, response_headers, status = http.request
+            {
+                url = self.url,
+                method = "POST",
+                headers =
+                {
+                    ["Connection"] =  "keep-alive",
+                    ["User-Agent"]= "Resurface/ " .. self.version .. " " .. "(" .. self.agent .. ")",
+                    ["Content-Type"] = "application/json; charset=UTF-8",
+                },
+                source = ltn12.source.string(msg),
+                sink = ltn12.sink.table(response_body)
+            }
+        if code == 204 then
+            return true
+        else
+            return false
+        end
+
+    end
 end
 
 
 -- 
-kwargs = {agent="usagelogger 0.1", url="https://httpbin.org/post"}
+kwargs = {agent="usagelogger 0.1", url="https://httpbin.org/status/204"}
 b = BaseLogger:new(kwargs)
-code, response_body = b:submit("hello")
-print("Response from server " .. table.concat(response_body))
+status = b:submit("hello")
+print(status)
+-- print("Response from server " .. table.concat(response_body))

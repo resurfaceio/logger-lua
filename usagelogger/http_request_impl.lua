e8c249ba1c32759763190e8d775fe56ba7615270
Anyesh <sthacruz@gmail.com>
2021-10-10 13:30:56 +0545

    refactor: Style methods

--- usagelogger/http_request_impl.lua
+++ usagelogger/http_request_impl.lua
@@ -1,16 +1,16 @@
 
 local HttpRequestImpl = {method="GET", url=nil, headers=nil, params=nil, body=nil}
 
-function HttpRequestImpl:new(o, method, url, headers, params, body)
-    o = o or {}
-    setmetatable(o, self)
-    self.__index = self
-    self.method = method or nil
-    self.url = url or nil
-    self.headers = headers or nil
-    self.params = params or nil
-    self.body = body or nil
-    return o
-end
+    function HttpRequestImpl:new(o, method, url, headers, params, body)
+        o = o or {}
+        setmetatable(o, self)
+        self.__index = self
+        self.method = method or nil
+        self.url = url or nil
+        self.headers = headers or nil
+        self.params = params or nil
+        self.body = body or nil
+        return o
+    end
 
 return HttpRequestImpl
--- usagelogger/http_response_impl.lua
+++ usagelogger/http_response_impl.lua
@@ -1,15 +1,14 @@
 
 local HttpResponseImpl = {status=nil, headers=nil, body=nil}
 
-
-function HttpResponseImpl:new(o, status, headers, body)
-    o = o or {}
-    setmetatable(o, self)
-    self.__index = self
-    self.status = status or nil
-    self.headers = headers or {}
-    self.body = body or nil
-    return o
-end
+    function HttpResponseImpl:new(o, status, headers, body)
+        o = o or {}
+        setmetatable(o, self)
+        self.__index = self
+        self.status = status or nil
+        self.headers = headers or {}
+        self.body = body or nil
+        return o
+    end
 
 return HttpResponseImpl
